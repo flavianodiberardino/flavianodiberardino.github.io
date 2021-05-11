1d51aae846ae1081b115e6820db10d7421e3fb08
Flaviano Di Berardino <69056903+flavianodiberardino@users.noreply.github.com>
2021-05-11 12:13:07 -0300

    fixing project descpription

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -7,18 +7,18 @@ const Project = () => {
     {
       name: 'TechStore',
       description: `This is an e-commerce where you can buy anything from computers to its 
-      components. It was made as the final project for Henry's Bootcamp. 
-      The tecnologies that we used are React-Redux, Supabase and also Node.js with Express.`,
-      link: 'https://www.linkedin.com/in/flavianodiberardino-js/',
-      github: 'https://github.com/flavianodiberardino'
+      components. It was made as the final project for Henry's Boot Camp. 
+      The technologies that we used are React-Redux, Supabase and also Node.js with Express.`,
+      link: 'https://henrystechstore.web.app/',
+      github: 'https://github.com/henry-labs/ecommerce-FT10-G1'
     },
     {
       name: 'World Video Games',
-      description: `This is an e-commerce where you can buy anything from computers to its 
-      components. It was made as the final project for Henry's Bootcamp. 
-      The tecnologies that we used are React-Redux, Supabase and also Node.js with Express.`,
+      description: `This is a simple app that i made as individual project for Henry's Boot Camp.
+      It's an app were you can find your favorite video games, you can see the its full info. I used React-Redux,
+      PostgreSQL, Sequelize, NodeJS and Express.`,
       link: 'https://www.linkedin.com/in/flavianodiberardino-js/',
-      github: 'https://github.com/flavianodiberardino'
+      github: 'https://github.com/flavianodiberardino/FT10-PI-Videogames'
     },
     // {
     //   name: 'TechStore',
